f5a9f1d44347c84de5997f3e45a1f13d48d197e4
Pritish Debnath <pritish.debnath@gmail.com>
2019-08-27 23:49:05 +0530

    First Commit

--- requests_start.py
+++ requests_start.py
@@ -1,15 +1,45 @@
 # importing the necessary library to make the call
 import requests
 
-def main ():
+
+def main():
+    # We are using get method to get request from the url
     url = "http://httpbin.org/xml"
     result = requests.get(url)
-    printResult(result)
+    # printresult(result)
 
-def printResult(resData):
+
+    # Post some data to the get endpoint using get method
+    url = "http://httpbin.org/get"
+    data = {
+        "key1": "Value1",
+        "key2": "Value2"
+    }
+    result = requests.get(url, params=data)
+    #printresult(result)
+
+    # Post some data to the get endpoint using post method
+    url = "http://httpbin.org/post"
+    data = {
+        "key1": "Value1",
+        "key2": "Value2"
+    }
+    result = requests.post(url, data=data)
+    #printresult(result)
+
+    # Post some data to the get endpoint using post method
+    url = "http://httpbin.org/get"
+    data = {
+        "User": "Pritish Debnath",
+    }
+    result = requests.get(url, headers=data)
+    printresult(result)
+
+
+def printresult(resData):
     print("Status Code: {0}".format(resData.status_code))
 
-    print ("Header ---------")
+    print("Header ---------")
     print(resData.headers)
     print("\n")
     print("Returned Data ----------")
@@ -18,4 +48,3 @@ def printResult(resData):
 
 if __name__ == "__main__":
     main()
-
